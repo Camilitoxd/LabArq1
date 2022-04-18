c99229575f45aaf6a28caa6a28436023df098c36
Camilo Gomez <Camilo Gomez@Laptop>
2022-04-18 17:34:09 -0500

    TestWs

---
+++ src/main/java/co/edu/unipiloto/ws/testws/services/Service.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.unipiloto.ws.testws.services;
+
+import co.edu.unipiloto.ws.testws.entidad.Person;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Camilo Gomez
+ */
+@Path("service")
+public class Service {
+
+    private static Map<Integer, Person> persons = new HashMap<Integer, Person>();
+
+    static {
+        for (int i = 0; i < 10; i++) {
+            Person person = new Person();
+            int id = i + 1;
+            person.setId(id);
+            person.setFullName("My wonderfull person " + id);
+            person.setAge(new Random().nextInt(40) + 1);
+            persons.put(id, person);
+        }
+
+    }
+
+    @GET
+    @Path("/getPersonByIdXML/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Person getPersonByIdXML(@PathParam("id") int id) {
+        return persons.get(id);
+    }
+
+    @GET
+    @Path("/getPersonByIdJson/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person getPersonByIdJson(@PathParam("id") int id) {
+        return persons.get(id);
+    }
+
+    @GET
+    @Path("/getAllPersonsInXML")
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Person> getAllPersonsInXML() {
+        return new ArrayList<Person>(persons.values());
+    }
+
+    @GET
+    @Path("/getAllPersonsInJson")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Person> getAllPersonsInJson() {
+        return new ArrayList<Person>(persons.values());
+    }
+
+    @GET
+    @Path("/getSalaryPromInXML")
+    @Produces(MediaType.APPLICATION_XML)
+    public int getAllSalaryInXML() {
+        int salaryProm = 0;
+        for (int i = 0; i < persons.size(); i++) {
+                salaryProm = persons.get(i).getSalario();
+        }
+        return salaryProm/persons.size();
+    }
+
+    @POST
+    @Path("/addPersonInJson")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Person addPersonInJson(Person person) {
+        System.out.println(person.getId());
+        persons.put(new Integer(person.getId()), person);
+        return person;
+    }
+}
